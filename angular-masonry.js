ff99bb233f40e5c0b87946b11ae89bbe6ef67fcd
bimal1331 <blueice.b13@gmail.com>
2014-06-24 18:57:44 +0530

    updating demo

--- angular-masonry.js
+++ angular-masonry.js
@@ -1,213 +1,188 @@
-angular.module('masonryLayout', [])
+(function() {
+	'use strict';
 
-.directive('masonryLayout', ['$window', function($window) {
+	angular.module('masonryLayout', [])
 
-	var dimensions, yMargin, xMargin, marginWidth, imgWidth, containers, midContainer, parentContainer, containerHeight, pageHeight;
+	.directive('masonry', ['$window', '$rootScope', function($window, $rootScope) {
 
-	function initialise() {
-		yMargin = dimensions.yMargin;
-		xMargin = dimensions.xMargin;
-		marginWidth = dimensions.marginWidth;
-		imgWidth = dimensions.imgWidth;
-		containers = dimensions.containers;
-	}
+		function Wall(xMargin, yMargin, imgWidth) {
 
-	return {
-		restrict: 'A',
+			this.IMG_MARGIN_X = xMargin;
 
-		require: '^masonryResize',
+			this.IMG_MARGIN_Y = yMargin;
 
-		link: function(scope, element, attrs, ctrl) {
-
-			var imageElem = element.find('img'),
-				tallestColumn, homeColumn, newLeft, newTop, newWidth;
-
-			element[0].style.cssText += "; left: -999px; top: -999px; opacity: 0; position:absolute; "
-
-			var repaint = function() {
+			this.IMG_WIDTH = imgWidth;
 			
-				homeColumn = ctrl.shortest();				
-				tallestColumn = ctrl.tallest();
-				newLeft = homeColumn*(imgWidth + xMargin) + marginWidth;
-				newTop = containers[homeColumn];
-
-				ctrl.update(homeColumn, element[0].scrollHeight + yMargin);
-				
-				element[0].style.cssText += "; left: " + newLeft + "px; top: " + newTop + "px; opacity: 1;";
-
-				if(++ctrl.imagesLoadCount === ctrl.totalItemCount) {
-					//this is the last image loaded
-					//correct parent height
-					console.log('Correcting parent height')
-					element.parent()[0].style.height = ctrl.tallest() + 'px';
-				}
-
-			};			
-
-			//Reset container height to 0 on view change(images count = 0)
-			if(scope.$index === 0) {
-				dimensions = ctrl.dimensions();
-				initialise();
-				containerHeight = 0;
-			}
-
-			if(scope.$last) {
-				ctrl.totalItemCount = scope.$index + 1;
-				element.parent()[0].style.height = ctrl.tallest() + (ctrl.docHeight()) + 'px'; 
-			}
-
-			//Set item position
-			imageElem.on('load', repaint);
-
-			imageElem.on('error', function() {
-				ctrl.imagesLoadCount++;
-			});
-
-		}
-	}
-}])
-
-.directive('masonryResize', ['$window', '$rootScope', function($window, $rootScope) {
-
-	var resizing = false,
-		dimensions;
-
-	var masonryData = $rootScope.masonryData,
-		IMG_WIDTH = masonryData.imgWidth,
-		IMG_MARGIN_X = masonryData.xMargin,
-		IMG_MARGIN_Y = masonryData.yMargin,
-		CONTAINER_PADDING_X = masonryData.containerPadding,
-		windowWidth = $window.innerWidth,
-		containerWidth, columns, dynamicImageWidth, marginWidth, containers, i, n;	
-
-	return {
-		restrict: 'A',
-
-		controller: ['$scope', '$element', function($scope, $element) {
-
 			this.imagesLoadCount = 0;
+
 			this.totalItemCount = 0;
 
-			this.shortest = function() {
-				return containers.indexOf(containers.slice().sort(function(a, b) {
+			this.resizing = false;
+
+			this.windowWidth = $window.innerWidth;
+
+			this.containers;
+
+			this.containerWidth;		
+
+			this.marginWidth;
+		}
+
+		Wall.prototype = {
+			constructor : Wall,		
+
+			docHeight : function() {
+				return $window.innerHeight * 2.5;
+			},
+
+			reset : function($element) {
+				var columns;
+
+				document.body.style.overflow = 'scroll';
+				this.containerWidth = $element[0].clientWidth;
+				document.body.style.overflow = 'auto';
+				columns = Math.floor((this.containerWidth + this.IMG_MARGIN_X)/(this.IMG_WIDTH + this.IMG_MARGIN_X));
+				this.marginWidth = Math.abs((this.containerWidth - this.IMG_WIDTH*columns - this.IMG_MARGIN_X*(columns - 1))/2);
+				this.containers = new Array(columns);
+
+				for(var i=0, n=this.containers.length; i < n; i++) {
+					this.containers[i] = 0;
+				}
+			},
+
+			setWindowWidth : function() {
+				this.windowWidth = $window.innerWidth;
+			},
+
+			shortest : function() {
+				return this.containers.indexOf(this.containers.slice().sort(function(a, b) {
 					return a - b;
 				})[0]);
-			};
+			},
 
-			this.tallest = function() {
-				return containers.slice().sort(function(a, b) {
+			shouldResize : function() {
+				return (Math.abs(this.windowWidth - $window.innerWidth) > 25) && !this.resizing ? true : false;
+			},
+
+			tallest : function() {
+				return this.containers.slice().sort(function(a, b) {
 					return b-a;
 				})[0];
-			};	
+			},
 
-			this.dimensions = function() {
-				return {
-					xMargin: IMG_MARGIN_X,
-					yMargin: IMG_MARGIN_Y,
-					windowWidth: windowWidth,
-					containers: containers,
-					imgWidth: IMG_WIDTH,
-					marginWidth: marginWidth
-				}
-			};
+			update : function(column, height) {
+				this.containers[column] += height;
+			}
 
-			this.update = function(column, height) {
-				containers[column] += height;
-			};
+		};
 
-			this.reset = function() {
-				document.body.style.overflow = 'scroll';
-				containerWidth = $element[0].clientWidth;
-				document.body.style.overflow = 'auto';
-				columns = Math.floor((containerWidth + IMG_MARGIN_X)/(IMG_WIDTH + IMG_MARGIN_X));
-				marginWidth = Math.abs((containerWidth - IMG_WIDTH*columns - IMG_MARGIN_X*(columns - 1))/2);
-				containers = new Array(columns);
-			};
+		return {
+			restrict: 'A',
 
-			this.init = function() {
-				for(i=0, n=containers.length; i < n; i++) {
-					containers[i] = 0;
-				}
-			};
+			link: function(scope, element, attrs, ctrl) {
 
-			this.shouldResize = function() {
-				return Math.abs(windowWidth - $window.innerWidth) > 25 ? true : false;
-			};
+				var wall = new Wall(+attrs.xMargin, +attrs.yMargin, +attrs.imgWidth),
+					imageContainers, container, homeColumn, newLeft, newTop;
 
-			this.setWindowWidth = function() {
-				windowWidth = $window.innerWidth;
-			};
+				var setNewCoordinates = function() {
+					homeColumn = wall.shortest();
+					newLeft = homeColumn*(wall.IMG_WIDTH + wall.IMG_MARGIN_X) + wall.marginWidth;
+					newTop = wall.containers[homeColumn];
+				};
 
-			this.docHeight = function() {
-				return $window.innerHeight * $rootScope.masonryData.falseDocumentHeight;
-			};	
-
-		}],
-
-		link: function(scope, element, attrs, ctrl) {
-
-			var imageContainers, container, homeColumn, newLeft, newTop;
-
-			var repaint = function() {
-
-				if(ctrl.shouldResize() && !resizing) {
-					resizing = true;
-					ctrl.setWindowWidth();
-
-					//Reset dimensions
-					ctrl.reset();
-					ctrl.init();
-					dimensions = ctrl.dimensions();
-
-					imageContainers = element[0].children;
-
-					for(i=0, n=imageContainers.length; i<n-1; i++) {
-						homeColumn = ctrl.shortest();
-						container = imageContainers[i];
-						newLeft = homeColumn*(dimensions.imgWidth + dimensions.xMargin) + dimensions.marginWidth;
-						newTop = dimensions.containers[homeColumn];
-
-						container.style.cssText += "; opacity: 1; left: " + newLeft + "px; top: " + newTop + "px;";
-
-						ctrl.update(homeColumn, container.scrollHeight + dimensions.yMargin);
-
-					}
-
-					element[0].style.height = ctrl.tallest() + 'px';
-					resizing = false;
-
-				}
-
-			};
-
-			scope.$watch(
-				function() {
-					return element[0].children.length;
-				}, 
-				function(newCount, oldCount) {
+				var repaint = function() {
 					
-					if(newCount === 0) {
-						element[0].style.height = 0;
-						ctrl.totalItemCount = 0;
-						ctrl.imagesLoadCount = 0;
-						ctrl.init();
+					if(wall.shouldResize()) {
+						
+						wall.resizing = true;
+						wall.setWindowWidth();
+
+						//Reset wall attributes
+						wall.reset(element);
+
+						imageContainers = element[0].children;
+
+						for(var i = 0, n = imageContainers.length; i < n; i++) {							
+							container = imageContainers[i];
+							
+							setNewCoordinates();
+
+							container.style.cssText += "; left: " + newLeft + "px; top: " + newTop + "px;";
+							wall.update(homeColumn, container.scrollHeight + wall.IMG_MARGIN_Y);
+						}
+
+						element[0].style.height = wall.tallest() + 'px';
+						wall.resizing = false;
+
 					}
 
-				}
-			);
+				};
 
-			angular.element($window).on('resize', repaint);
+				var fixBrick = function(brick) {
+					setNewCoordinates();	
+					
+					brick.style.cssText += "; left: " + newLeft + "px; top: " + newTop + "px;";
+					wall.update(homeColumn, brick.scrollHeight + wall.IMG_MARGIN_Y);
 
-			scope.$on('$destroy', function() {
-				resizing = false;
-				angular.element($window).off('resize');
-			});
+					if(++wall.imagesLoadCount === wall.totalItemCount) {
+						//this is the last image loaded
+						//correct parent height
+						element[0].style.height = wall.tallest() + 'px';
+					}
 
-			element[0].style.position = 'relative';
+				};
 
-			ctrl.reset();
-			ctrl.init();
+				var attachListener = function(brick) {
+					//Create closure for image containers i.e bricks
+					var imgElem = brick.getElementsByTagName('img')[0];
+					brick.style.cssText += "; left: -999px; top: -999px; position:absolute; ";
 
+					imgElem.addEventListener('load', function() {
+						fixBrick(brick);
+					});
+
+					imgElem.addEventListener('error', function() {
+						wall.imagesLoadCount++;
+					});
+				};
+
+				scope.$watch(
+					function() {
+						return element[0].children.length;
+					}, 
+					function(newCount, oldCount) {
+
+						if(newCount === oldCount) return;
+						
+						if(oldCount === 0) {
+							element[0].style.height = 0;
+							wall.totalItemCount = 0;
+							wall.imagesLoadCount = 0;
+
+							//Reset wall attributes
+							wall.reset(element);
+						}
+
+						wall.totalItemCount = newCount;
+						element[0].style.height = wall.tallest() + (wall.docHeight()) + 'px'; 
+
+						for(var i = oldCount; i < newCount; i++) {
+							attachListener(element[0].children[i]);						
+						}
+
+					}
+				);
+
+				angular.element($window).on('resize', repaint);
+
+				scope.$on('$destroy', function() {
+					angular.element($window).off('resize', repaint);
+				});
+
+				element[0].style.position = 'relative';
+
+			}
 		}
-	}
-}])
+	}])
+
+}());
